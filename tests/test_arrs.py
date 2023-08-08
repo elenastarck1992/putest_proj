f7701d9c87196bb3d9f7709c35a7381e1333663d
elena <elenastarck1992@mail.ru>
2023-08-08 20:02:47 +0300

    asserts added

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,18 @@
+import pytest
+
 from utils import arrs
+
+# @pytest.fixture
+# def coll_for_tests():
+#     return
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], -1, "test") == "test"
+    assert arrs.get([], 0, "test") != "test"
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 4]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
